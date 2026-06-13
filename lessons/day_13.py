41c541efdc02ab353316e1056e109f0797ef811a
Sergey <mr.prohindei@gmail.com>
2026-06-13 22:44:23 +0300

    День 14

--- lessons/day_13.py
+++ lessons/day_13.py
@@ -12,14 +12,22 @@
 # Ниже вызови её: спроси у пользователя ширину и высоту (int),
 # передай в функцию, результат выведи одной строкой:
 #   Площадь: 50
-
+def rectangle_area(width, height):
+    return width * height
+x=int(input("Введите ширину: "))
+y=int(input("Введите высоту: "))
+result = rectangle_area(x, y)
+print("Площадь:", result)
 
 # --- Задача B ---
 # Напиши функцию greeting(name) — печатает: Привет, ...!
 # (имя подставляется из параметра)
 # Вызови её три раза с разными именами (строками в коде, без input).
-
-
+def greeting(name):
+    print("Привет!", name)
+greeting("Sergey")
+greeting("Alexey")
+greeting("Ivan")
 # --- Задача C (опционально) ---
 # Напиши функцию ticket_price(age):
 #   если age < 12  → return 0
@@ -27,7 +35,16 @@
 #   иначе          → return 400
 # Спроси возраст у пользователя, вызови функцию, выведи:
 #   Стоимость билета: ...
-
+def ticket_price(age):
+    if age < 12:
+        return 0
+    elif age >= 65:
+        return 200
+    else:
+        return 400
+x = int(input("Введите возраст: "))
+f = ticket_price(x)
+print ("Стоимость билета =", f)
 
 # --- Задача D ---
 # Сборка — в отдельном файле: problem_D/day_13D.py
